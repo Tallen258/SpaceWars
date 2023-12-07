710d88bbcfef235e98aaa37511de581cf80f73f9
Breanna Nesbit <brebrenesbit02@gmail.com>
2023-12-07 16:24:22 -0700

    added heading to joinGame Response

--- src/SpaceWars.Web.Types/Records.cs
+++ src/SpaceWars.Web.Types/Records.cs
@@ -1,6 +1,6 @@
 ﻿namespace SpaceWars.Web.Types;
 
-public record JoinGameResponse(string Token, Location StartingLocation, string GameState, int BoardWidth, int BoardHeight);
+public record JoinGameResponse(string Token, Location StartingLocation, string GameState, int Heading, int BoardWidth, int BoardHeight);
 public record Location(int X, int Y);
 public record GameStateResponse(string GameState, IEnumerable<Location> PlayerLocations);
 public record PlayerMessageResponse(string Type, string Message);
